dda36d706fdbb58269e1cde9c90778135d7d9b9e
Bienvenu Cyuzuzo <cbienvenu007@gmail.com>
2024-10-10 10:39:04 +0200

    declare a winner if not return null

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
-  const [squares, useSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -23,12 +23,21 @@ export default function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    useSquares(nextSquares);
+    setSquares(nextSquares);
     setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner is: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <div className="status">{ status }</div>
       <div className="board-row">
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -48,4 +57,25 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [ // all possible winning combinations 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) { // loop through to determine the winner
+    const [a, b, c] = lines[i]; // destructure the winning combination
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { // check if there's a winner and return the winner
+      return squares[a];
+    }
+  }
+  return null;
 }
